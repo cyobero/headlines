8b42bed1ff6ede94b60b7c0403a3ecfe8849ef13
czar <czar.yobero@gmail.com>
2021-05-20 20:14:43 -0700

    ignore pycache

--- headlines/__init__.py
+++ headlines/__init__.py
@@ -12,18 +12,19 @@ RSS_FEEDS = {
 }
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def get_news(publication="bbc"):
     """
     Returns RSS feed for given publication.
     """
-    query = request.args.get("publication")
+    query = request.args.get('publication')
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("index.html", articles=feed['entries'])
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
