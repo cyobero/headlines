59b5a6538a301e4c743f27ad24c44163d61b148d
czar <czar.yobero@gmail.com>
2021-05-21 14:26:38 -0700

    currency converter

--- headlines/__init__.py
+++ headlines/__init__.py
@@ -13,13 +13,32 @@ RSS_FEEDS = {
     'iol': 'http://iol.co.za/cmlink/1.640'
 }
 
-DEFAULTS = {'publication': 'bbc', 'city': 'London, UK'}
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London, UK',
+            'currency_from': 'USD',
+            'currency_to': 'EUR'}
+
+WEATHER_APP_ID = '758501f8c3d9c9c51884c5e7bee1e523'
+CURRENCY_APP_ID = '1dbf2bb23f364a0890aca9f39215e95b'
+
+WEATHER_URL ='http://api.openweathermap.org/data/2.5/weather?q={0}&units=imperial&appid={1}'
+CURRENCY_URL = 'https://openexchangerates.org/api/latest.json?app_id={}'.format(
+    CURRENCY_APP_ID)
+
+
+def get_rate(currency1, currency2):
+    """Returns the exchange of two different currencies."""
+    all_currency = urllib.request.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    currency1_rate = parsed.get(currency1.upper())
+    currency2_rate = parsed.get(currency2.upper())
+    return currency2_rate/currency1_rate
 
 
 def get_weather(query):
+    """Returns the current weather (in fahrenheit) of a given city."""
     query = urllib.parse.quote(query)
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=758501f8c3d9c9c51884c5e7bee1e523'.format(
-        query)
+    url = WEATHER_URL.format(query, WEATHER_APP_ID)
     data = urllib.request.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
@@ -49,11 +68,21 @@ def home():
     if not publication:
         publication = DEFAULTS['publication']
     articles = get_news(publication)
+
     city = request.args.get('city')
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template('index.html', articles=articles, weather=weather)
+
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate = get_rate(currency_from, currency_to)
+    return render_template('index.html', articles=articles, weather=weather,
+                           currency_from=currency_from, currency_to=currency_to, rate=rate)
 
 
 if __name__ == "__main__":
